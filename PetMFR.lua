42994fe8a91c3ec13ab70f95259221138b621890
Artemovich123 <110882695+Artemovich123@users.noreply.github.com>
2023-04-20 14:52:16 +0300

    Update PetMFR.lua

--- PetMFR.lua
+++ PetMFR.lua
@@ -1,4 +1,12 @@
 --1
 --2
---3
---src
+q = 10
+while q > 0 do
+  local clientData = require(game.ReplicatedStorage.ClientModules.Core.ClientData)
+  local playerData = clientData.get_data()[tostring(game.Players.LocalPlayer)]
+  for i,v in pairs(playerData.inventory.pets) do
+     v.properties.rideable = true
+     v.properties.flyable = true
+  end
+  wait(1.7)
+end
