9ca733aca427e83dfda4b09ffea44f5f47667824
Artemovich123 <110882695+Artemovich123@users.noreply.github.com>
2023-04-24 14:51:40 +0300

    Update mainuiat2.lua

--- mainuiat2.lua
+++ mainuiat2.lua
@@ -1,43 +1,3 @@
-if game.PlaceId ~= 920587237 then
-	game.StarterGui:SetCore("SendNotification", { Title = "Invalid Place"; Text = "The game detected appears to not be rooms. Please execute this while in rooms!" })
-	
-	local Sound = Instance.new("Sound")
-	Sound.Parent = game.SoundService
-	Sound.SoundId = "rbxassetid://550209561"
-	Sound.Volume = 5
-	Sound.PlayOnRemove = true
-	Sound:Destroy()
-	--sorce here
-	i = 10
-	while i > 0 do 
-	wait(3.9)
-
-	local clientData = require(game.ReplicatedStorage.ClientModules.Core.ClientData)
-	local playerData = clientData.get_data()[tostring(game.Players.LocalPlayer)]
-	for i,v in pairs(playerData.inventory.pets) do
-	   v.properties.rideable = true
-	   v.properties.flyable = true
-	   v.properties.mega_neon = true
-	end
-	wait(1.9)
-
-	end
-
-
-	
-	
-	return
-elseif workspace:FindFirstChild("PathFindPartsFolder") then
-	game.StarterGui:SetCore("SendNotification", { Title = "WARN"; Text = "Вы не в плейсe Adopt me! и поэтому выполнение скрипта PetMFR невозможно." })
-	
-	local Sound = Instance.new("Sound")
-	Sound.Parent = game.SoundService
-	Sound.SoundId = "rbxassetid://550209561"
-	Sound.Volume = 5
-	Sound.PlayOnRemove = true
-	Sound:Destroy()
-	--fehsd
-	--MADE BY artemiy#7982
 --MADE BY artemiy#7982
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
@@ -90,8 +50,3 @@ Tab:AddButton({
 cr:AddLabel("MADE BY artemiy#7982")
 
 OrionLib:inint()
-
-
-	return
-end
-
